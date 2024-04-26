a96d15dc907c9d83850fca229ce2d3dd05251954
anissarkeraraf <anissarkeraraf@gmail.com>
2024-04-26 20:44:58 +0600

    added the post method

--- index.js
+++ index.js
@@ -27,13 +27,31 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const spotsConnection = client.db('spotdb').collection('spot');
+
+
+    app.get('/spot', async(req, res) => {
+        const cursor = spotsConnection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.post('/spot', async(req, res) => {
+        const touristSpots = req.body;
+        console.log(touristSpots)
+        const result = await spotsConnection.insertOne(touristSpots);
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
