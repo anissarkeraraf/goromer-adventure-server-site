b2319075c9b45574f12d8711571f9ad33aacc1ad
anissarkeraraf <anissarkeraraf@gmail.com>
2024-04-29 16:33:36 +0600

    added the user post method

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const spotsCollection = client.db('spotdb').collection('spot');
     const userCollection = client.db('spootdb').collection('user');
